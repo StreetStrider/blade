eddcaf4274a3790d3b3db1348c6007ad33f71990
Strider <strange.transistor@gmail.com>
2019-05-14 01:27:00 +0300

    test empty.

---
+++ test/empty.test.js
@@ -0,0 +1,19 @@
+
+import { expect } from 'chai'
+
+import empty from '../empty'
+
+
+describe('empty', () =>
+{
+	it('empties', () =>
+	{
+		var $body = document.body
+
+		$body.innerHTML = `<div>1</div><div><span>2</span></div>3`
+
+		empty($body)
+
+		expect($body.innerHTML).eq('')
+	})
+})
