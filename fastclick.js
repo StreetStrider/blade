6bc6963a2cfdbc3354ec36b1f71308ed1e2f33c3
Strider <strange.transistor@gmail.com>
2019-06-16 16:44:27 +0300

    fastclick: passive is not required.

--- fastclick.js
+++ fastclick.js
@@ -4,7 +4,5 @@ import on from 'event'
 
 export default function fastclick (element, handler, options)
 {
-	options = { ...options, passive: true }
-
 	return on(element, 'mousedown', handler, options)
 }
