d0b8a537c904efe637ba5b2abb7266a3b016f5b2
Strider <strange.transistor@gmail.com>
2019-06-16 16:56:11 +0300

    fastclick: handle keyboard and preventDefault for all.

--- fastclick.js
+++ fastclick.js
@@ -1,8 +1,31 @@
 
+import { compose } from 'rambda/src/compose'
+
 import on from './event'
 
 
 export default function fastclick (element, handler, options)
 {
-	return on(element, 'mousedown', handler, options)
+	function mousedown (e)
+	{
+		e.preventDefault()
+
+		handler(e)
+	}
+
+	function click (e)
+	{
+		e.preventDefault()
+
+		/* keyboard event */
+		if (! e.detail)
+		{
+			handler(e)
+		}
+	}
+
+	var md = on(element, 'mousedown', mousedown, options)
+	var cl = on(element, 'click', click, options)
+
+	return compose(cl, md)
 }
