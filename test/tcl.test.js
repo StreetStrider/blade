1906332bf071dcbcf60e2721107bebe1d125f216
Strider <strange.transistor@gmail.com>
2019-07-16 00:41:53 +0300

    tcl: test tcls.

--- test/tcl.test.js
+++ test/tcl.test.js
@@ -4,13 +4,11 @@
 import { expect } from 'chai'
 
 import tcl from '../tcl'
-// import { tcls } from '../tcl'
+import { tcls } from '../tcl'
 
 
 describe('tcl', () =>
 {
-	var $body = document.body
-
 	it('(E, C, S)', () =>
 	{
 		var $ = $div()
@@ -88,16 +86,77 @@ describe('tcl', () =>
 		ex_cl($1, 'foo')
 		ex_cl($2, 'foo')
 	})
-
-	function $div ()
-	{
-		$body.innerHTML = `<div></div>`
-
-		return $body.querySelector('div')
-	}
-
-	function ex_cl ($, cl)
-	{
-		expect($.className).eq(cl)
-	}
 })
+
+
+describe('tcls', () =>
+{
+	it('string', () =>
+	{
+		var $ = $div()
+
+		tcls($, 'foo')
+		ex_cl($, 'foo')
+
+		tcls($, 'foo', false)
+		ex_cl($, '')
+
+		tcls($, 'foo', null)
+		ex_cl($, 'foo')
+
+		tcls($, 'foo', null)
+		ex_cl($, '')
+	})
+
+	it('array ()', () =>
+	{
+		var $ = $div()
+
+		tcls($, [ 'foo', 'bar' ])
+		ex_cl($, 'foo bar')
+
+		tcls($, [ 'foo', 'bar' ])
+		ex_cl($, 'foo bar')
+	})
+
+	it('array (true)', () =>
+	{
+		var $ = $div()
+
+		tcls($, [ 'foo', 'bar' ], true)
+		ex_cl($, 'foo bar')
+	})
+
+	it('array (false)', () =>
+	{
+		var $ = $div()
+
+		tcls($, [ 'foo', 'bar', 'baz' ], true)
+		tcls($, [ 'bar' ], false)
+		ex_cl($, 'foo baz')
+	})
+
+	it('array (null)', () =>
+	{
+		var $ = $div()
+
+		tcls($, [ 'foo', 'bar' ])
+		tcls($, [ 'bar', 'baz' ], null)
+		ex_cl($, 'foo baz')
+	})
+})
+
+
+var $body = document.body
+
+function $div ()
+{
+	$body.innerHTML = `<div></div>`
+
+	return $body.querySelector('div')
+}
+
+function ex_cl ($, cl)
+{
+	expect($.className).eq(cl)
+}
